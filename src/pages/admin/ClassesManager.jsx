8e3cc87f53a559cbe3750cd36c0156751dbf7ef4
Dinh Thien <vudinhthien719@gmail.com>
2025-05-02 10:20:09 +0700

    modified ClassesManager.jsx

--- src/pages/admin/ClassesManager.jsx
+++ src/pages/admin/ClassesManager.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import BASE_URL from '../components/BaseURL';
+import BASE_URL from "../../components/BaseURL";
 
 export default function ClassesManager() {
   const [classes, setClasses] = useState([]);
